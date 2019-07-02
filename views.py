b1f6452694b7113c8820deed716f33444a416f54
billmanh <william.jeffrey.harding@gmail.com>
2019-07-02 11:41:07 +0000

    moved login window, plus created main login (outside of repo) to manage users

--- views.py
+++ views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def login(request):
-    return render(request,'login.html')
+    return render(request,'game/start_screen.html')
+
+
