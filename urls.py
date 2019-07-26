a4bf2bfbb241da2cd91450844666ba104cbd38a5
billmanh <william.jeffrey.harding@gmail.com>
2019-07-26 20:35:48 +0000

    some minor cleanup, spellcheck etc

--- urls.py
+++ urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
         path('createworld01', views.create_world_01, name='create_world_01'),
         path('showworld01', views.show_world_01, name='show_world_01'),
         path('createworld02', views.create_world_02, name='create_world_02'),
-        path('showworld02', views.show_world_02, name='show_world_02')
+        path('showworld02', views.show_world_02, name='show_world_02'),
+        path('showworld03', views.show_world_03, name='show_world03')
         ]
+
--- views.py
+++ views.py
@@ -46,6 +46,10 @@ def show_world_02(request):
         context['formData']  = yaml.load(request.POST.get("formData","No data found"),yaml.SafeLoader)
         world = b.get_world(request.user.get_username())
         world = the_second_age(world,context['formData'])
+        #post the `world` to an s3 bucket, using the username as the key
+        user = request.user.get_username()
+        b.save_world(world,user)
+        #After the world is saved we can change the df for display.   
         world.df_features = world.df_features.fillna('')
         #building a dictionairy in the format that d3.js will prefer
         wa = [world.df_features.loc[m].to_dict() for m in world.df_features.index]
@@ -55,3 +59,18 @@ def show_world_02(request):
         return render(request,'game/show_world02.html',context)
     else:
         return render(request, 'game/create_world02.html')
+
+
+@login_required
+def show_world_03(request):
+    if "POST" == request.method:
+        context = {'phase':3}
+        world = b.get_world(request.user.get_username())
+        world = the_third_age(world)
+        #post the `world` to an s3 bucket, using the username as the key
+        user = request.user.get_username()
+        b.save_world(world,user)
+        #building a dictionairy in the format that d3.js will prefer
+        return render(request,'game/show_world03.html',context)
+    else:
+        return render(request, 'game/show_world02.html')
