889d0a4f97b670d0f77b47af8d5733638a1fe258
billmanh <william.jeffrey.harding@gmail.com>
2019-08-04 20:06:52 +0000

    added some models for terrain and monsters

--- lib/modify_character.py
+++ lib/modify_character.py
@@ -8,14 +8,17 @@ class Character():
         self.background = f['background']
         self.coreskills = [f['coreskills']]
         self.secondaryskills = [f['secondaryskills']]
-        
+        self.message = "game start" 
+        self.equipment = {'gold':1}
+
     def __repr__(self):
         return f"{self.name} the adventurer"
-    
+           
     def get_charData(self):
         #try to pass more on the server side. Don't pass info to the client unless there is .js that needs it. 
         return {
-            'name':self.name
+            'name':self.name,
+            'message':self.message
         }
     
     def get_location_key(self):
@@ -26,35 +29,42 @@ class Character():
         l = self.location
         return {'x':l[0],'y':l[1]}
 
-def char_origin(c,world):
+def set_char_origin(c,world):
     # a noble is born and raised in a capitol
     if c.background == 'Noble family':
         c.caste = "noble"
         t = np.random.choice([t for t in world.towns if 'capitol' in str(t)])
         c.birthplace = t.name
         c.location = [t.x,t.y]
+        c.equipment['gold'] += 100
+        c.message = f"You begin your adventure in {str(t)} </br> Your nobile family has ties with {t.speaker}. "
+
     if c.background == 'Pesant family':
         c.caste = "pesant"
         t = np.random.choice([t for t in world.towns if 'town' in str(t)])
         c.birthplace = t.name
         c.location = [t.x,t.y]
+        c.message = f"You wake up on the streets of {str(t)}."
+        
     if c.background == 'Temple orphan':
         c.caste = "pesant"
         t = np.random.choice([t for t in world.towns if 'town' in str(t)])
         c.birthplace = t.name
         c.location = [t.x,t.y]
+        c.message = f"You wake up on the streets of {str(t)}."
+
     if c.background == 'Nomad family':
         c.caste = "pesant"
         l = world.get_filtered_chord(t=['mountain','land'])
         c.birthplace = "the wilderness"
         c.location = l
-
+        c.message = f"You wake up in the wilderness, {str(world.df_features.loc[c.get_location_key(),'terrain'])}."
         
 #creating the character from the userform (f)
 #returns a world, not a character
 def create_character(character_dict,world):
     c = Character(character_dict,world)
-    char_origin(c,world)
+    set_char_origin(c,world)
     world.Character = c
     
     return world
--- models.py
+++ models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class terrain_items(models.Model):
+    name = models.CharField(max_length=50,primary_key=True)
+    hexcolor = models.CharField(max_length=50)
+    size = models.IntegerField()
+    spread = models.CharField(max_length=50)
+    affect = models.CharField(max_length=50)
+    size = models.IntegerField()
+    affectText = models.CharField(max_length=250)
+
+class terrain_details(models.Model):
+   name = models.CharField(max_length=50,primary_key=True)
+   land_type = models.CharField(max_length=50)
+   description = models.CharField(max_length=250)
+   creatures = models.Charfield(max_length=250)
+   terrain_textures = models.Charfield(max_length=500)
+
+
+class bestiary(models.Model):
+    key = models.CharField(max_length=100,primary_key=True)
+    name = models.CharField(max_length=100,primary_key=True)
+    health = models.IntegerField()
+    healthMaxVariance = models.IntegerField()
+    move = models.IntegerField()
+    size = models.IntegerField()
+    move_type = models.CharField(max_length=50)
+    render_type = models.CharField(max_length=50)
+    group_min = models.IntegerField()
+    group_max = models.IntegerField()
+    color = models.CharField(max_length=50)
+    perception = models.IntegerField()
+    attack_type = models.Charfield(max_length=250)
+    damage = models.IntegerField()
+
+
