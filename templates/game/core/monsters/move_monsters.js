765bb0bf36c6198f9a693ec3b7fc82b466d8569b
billmanh <william.jeffrey.harding@gmail.com>
2019-09-24 11:47:35 +0000

    cleaned up the movement code

--- templates/game/core/monsters/move_monsters.js
+++ templates/game/core/monsters/move_monsters.js
@@ -5,20 +5,28 @@ function move_monster(){
         if (d3.select(this).classed("detectsPlayer")==false){
                 objectAlerts("#"+d3.select(this).attr("id"),d3.select(this).attr("name")+" Has spotted " + charData["name"]);
                 d3.select(this).classed("detectsPlayer",true);
-            }
-        if(get_dist_to_char(d3.select(this).attr("cx"),d3.select(this).attr("cy"))<=charData["size"]){
-               {% include "game/core/monsters/attack_monster.js" %}
-            }
-            nmc = move_towards_obj(Math.round(d3.select(this).attr("cx")),
+        }nmc = move_towards_obj(Math.round(d3.select(this).attr("cx")),
                         Math.round(d3.select(this).attr("cy")),
                         char_x,
                         char_y,
                         d3.select(this).attr("move"));
-            d3.select(this).transition()
-                .attr("cx",nmc[0])
-                .attr("cy",nmc[1])
-        }
-    else{
+                d3.select(this).transition()
+                    .attr("cx",nmc[0])
+                    .attr("cy",nmc[1])
+
+            if(get_dist_to_char(d3.select(this).attr("cx"),d3.select(this).attr("cy"))<=charData["size"]){
+                nmc[0] = nmc[0]*-2
+                nmc[1] = nmc[1]*-2
+                d3.select(this).transition()
+                    .attr("cx",nmc[0])
+                    .attr("cy",nmc[1])
+            {% include "game/core/monsters/attack_monster.js" %}
+            }else{
+                d3.select(this).transition()
+                    .attr("cx",nmc[0])
+                    .attr("cy",nmc[1])
+            }
+        }else{
         d3.select(this)
         .transition()
                 .attr("cx",function(d, i) { return (parseInt(d3.select(this).attr("cx")))+(coordshifter(3))})
