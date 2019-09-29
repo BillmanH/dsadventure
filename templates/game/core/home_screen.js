e56cae6e5676e303c39e48d1526f6799e477cd76
billmanh <william.jeffrey.harding@gmail.com>
2019-09-29 15:13:38 +0000

    updated the screen width on desktop

--- templates/game/core/home_screen.js
+++ templates/game/core/home_screen.js
@@ -2,7 +2,7 @@ var widthPadding = 200
 if(window.innerWidth <= 800){
      widthPadding=15;
    }
-    var width = screen.width-widthPadding,
+    var width = window.innerWidth-widthPadding,
         height = window.innerHeight-100,
         char_x = screen.width/2,
         char_y = screen.height / 2;
@@ -13,3 +13,5 @@ if(window.innerWidth <= 800){
         .attr("id","screen")
         .attr("z-index","1")
         .style("border", "1px solid black");
+
+
