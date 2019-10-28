4cce64b78b699e0f49911a3cb18cd9360d517bb3
billmanh <william.jeffrey.harding@gmail.com>
2019-10-28 10:27:36 +0000

    grass added a line

--- templates/game/core/terrain/grass.js
+++ templates/game/core/terrain/grass.js
@@ -1,1 +1,16 @@
-{% extends 'game/core/terrain/render_scatter.js' %}
+{% include 'game/core/terrain/render_scatter.js' %}
+
+ter_group_grass.selectAll()
+        .data(function(d){return d})
+        .enter()
+        .append("line")
+                    .style('z-index',-1)
+                    .attr("x1",function(d){return d.spawnOrigin_x})
+                    .attr("x2",function(d){return d.spawnOrigin_x})
+                    .attr("y1",function(d){return d.spawnOrigin_y})
+                    .attr("y2",function(d){return d.spawnOrigin_y - (d.size + 5)})
+                    .attr("name",function(d){return d.name})
+                    .classed("terrain", true)
+                    .style("stroke",function(d){return d.borderhex})
+                    .attr("stroke-width", 2)
+ 
