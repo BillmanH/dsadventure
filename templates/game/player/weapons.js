151bdefb4eacb07e1846ef9efbfa51c78690a206
billmanh <william.jeffrey.harding@gmail.com>
2019-12-04 05:36:14 +0000

    added first weapons rect

--- templates/game/player/weapons.js
+++ templates/game/player/weapons.js
@@ -2,12 +2,22 @@ var weapons = {{ weapons | safe }}
 
 console.log(weapons)
 
-var window_eq = d3.select("body")
+var window_weapons = d3.select("body")
     .append("svg")
     .attr("width",400)
     .attr("height",weapons.length * 100)
     .attr("id","screen")
     .attr("z-index","1")
     .style("border", "1px solid black");
+
+window_weapons.selectAll()
+        .data(weapons)
+        .enter()
+        .append("rect")
+        .attr("x",0)
+    .attr("y",weapons.length * 100)
+    .attr("height",100)
+    .attr("width",380)
+    .style("border", "1px solid black")
 
 
