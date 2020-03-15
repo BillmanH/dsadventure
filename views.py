1f04a6fe60e7cc5f8299d8c8e1fae9885594ddea
William Harding <williamh@metia.com>
2020-03-14 17:48:46 -0700

    Removed old files and added third age

--- views.py
+++ views.py
@@ -189,6 +189,15 @@ def create_world_01(request):
             world = b.get_world(request.user.get_username())
             world = the_second_age(world, context['formData'])
             b.save_world(world, user)
+        if context['formData']['phase'] == 3:
+            world = b.get_world(request.user.get_username())
+            world, events = the_third_age(world)
+            b.save_world(world, user)
+            context['events'] = events
+            context['rulers'] = [str(n.ruler) for n in world.nations]
+            context['loyalty'] = [str(n) for n in world.nations]
+            return render(request, 'game/show_world03.html', context)
+
         wa = [world.df_features.loc[m].fillna("").to_dict()
               for m in world.df_features.index]
         context['df_features'] = wa
@@ -197,68 +206,3 @@ def create_world_01(request):
         if 'continue' in context['formData'].keys():
             context['formData'].pop('continue')
         return render(request, 'game/generate_world.html', context)
-
-
-@login_required
-def show_world_01(request):
-    if "POST" == request.method:
-        context = {'phase': 1}
-        context['formData'] = yaml.load(request.POST.get(
-            "formData", "No data found"), yaml.SafeLoader)
-        world = the_first_age({})
-        # post the `world` to an s3 bucket, using the username as the key
-        user = request.user.get_username()
-        b.save_world(world, user)
-        # building a dictionairy in the format that d3.js will prefer
-        wa = [world.df_features.loc[m].to_dict()
-              for m in world.df_features.index]
-        context['df_features'] = wa
-        context['dim_1'] = np.unique(world.df_features['x']).tolist()
-        context['dim_2'] = np.unique(world.df_features['y']).tolist()
-        return render(request, 'game/show_world01.html', context)
-    else:
-        return render(request, 'game/show_world01.html')
-
-
-@login_required
-def create_world_02(request):
-    return render(request, 'game/create_world02.html')
-
-
-@login_required
-def show_world_02(request):
-    if "POST" == request.method:
-        context = {'phase': 2}
-        context['formData'] = yaml.load(request.POST.get(
-            "formData", "No data found"), yaml.SafeLoader)
-        world = b.get_world(request.user.get_username())
-        world = the_second_age(world, context['formData'])
-        # post the `world` to an s3 bucket, using the username as the key
-        user = request.user.get_username()
-        b.save_world(world, user)
-        # After the world is saved we can change the df for display.
-        world.df_features = world.df_features.fillna('')
-        # building a dictionairy in the format that d3.js will prefer
-        wa = [world.df_features.loc[m].to_dict()
-              for m in world.df_features.index]
-        context['df_features'] = wa
-        context['dim_1'] = np.unique(world.df_features['x']).tolist()
-        context['dim_2'] = np.unique(world.df_features['y']).tolist()
-        return render(request, 'game/show_world02.html', context)
-    else:
-        return render(request, 'game/create_world02.html')
-
-
-@login_required
-def show_world_03(request):
-    context = {'phase': 3}
-    world = b.get_world(request.user.get_username())
-    world, events = the_third_age(world)
-    # post the `world` to an s3 bucket, using the username as the key
-    user = request.user.get_username()
-    b.save_world(world, user)
-    # building a dictionairy in the format that d3.js will prefer
-    context['events'] = events
-    context['rulers'] = [str(n.ruler) for n in world.nations]
-    context['loyalty'] = [str(n) for n in world.nations]
-    return render(request, 'game/show_world03.html', context)
