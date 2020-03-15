85b77cbe3b00b3a1f347f40f0385426be9e59162
William Harding <williamh@metia.com>
2020-03-14 17:53:05 -0700

    updated urls

--- urls.py
+++ urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('startscreen', views.start_screen, name='login'),
     path('generateworld', views.generate_world, name='generate_world'),
-    path('showworld03', views.show_world_03, name='show_world03'),
     path('createcharacter', views.create_character, name='create_character'),
     path('coreview', views.core_view, name='core_view'),
     path('charmap', views.char_map, name='char_map'),
