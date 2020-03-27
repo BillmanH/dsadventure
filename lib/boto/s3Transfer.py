9c061413080443565303637b83b3564457563451
William Harding <william.jeffrey.harding@gmail.com>
2020-03-27 07:20:16 -0700

    updated safe_loader

--- lib/boto/s3Transfer.py
+++ lib/boto/s3Transfer.py
@@ -1,7 +1,7 @@
 from boto.s3.connection import S3Connection
 import boto
 import yaml
-#note to self, use the Loader=SafeLoader 
+# note to self, use the Loader=SafeLoader
 import numpy as np
 import pandas as pd
 import pickle
@@ -13,23 +13,25 @@ import os
 try:
     conn = S3Connection()
 except:
-    #for local loading, probably removed in production
-    myKeys = yaml.load(open(r'C:\Users\willi\OneDrive\Documents\keyfile.txt', 'r'))
-    AWSSecretKey=myKeys['AWSSecretKey']
-    AWSAccessKeyId=myKeys['AWSAccessKeyId']
+    # for local loading, probably removed in production
+    myKeys = yaml.safe_load(
+        open(r'C:\Users\willi\OneDrive\Documents\keyfile.txt', 'r'))
+    AWSSecretKey = myKeys['AWSSecretKey']
+    AWSAccessKeyId = myKeys['AWSAccessKeyId']
     conn = S3Connection(AWSAccessKeyId, AWSSecretKey)
 
-def save_world(world,user):
+
+def save_world(world, user):
     conn = S3Connection()
     mybucket = conn.get_bucket('dsadventure')
-    #save the bucket locally 
+    # save the bucket locally
     place = os.listdir()
-    pickled_world = open('game/pickles/'+ user + 'world.pkl', 'wb')
+    pickled_world = open('game/pickles/' + user + 'world.pkl', 'wb')
     pickle.dump(world, pickled_world)
     #myKey = mybucket.get_key('world/' + user + 'world.pkl')
-    
+
+
 def get_world(user):
-    with (open('game/pickles/'+ user + 'world.pkl', 'rb')) as pickle_file:
+    with (open('game/pickles/' + user + 'world.pkl', 'rb')) as pickle_file:
         world = pickle.load(pickle_file)
     return world
-
