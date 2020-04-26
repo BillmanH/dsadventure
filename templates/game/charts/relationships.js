d14a3d36ce27b64b0f61516fc2d4b0de91ac35eb
William Harding <william.jeffrey.harding@gmail.com>
2020-04-26 12:46:43 -0700

    New relationships graphic

--- templates/game/charts/relationships.js
+++ templates/game/charts/relationships.js
@@ -1,60 +1,65 @@
-var relationships = {
-    'name': 'Character',
-    'type': 'person',
-    'role': 'hero',
-    'children': {{ relationships | safe }}}
+var relationships = charData
+relationships['children'] = {{ relationships | safe }}
+relationships['role'] = 'hero'
+relationships['type'] = 'person'
+
+// var relationships = {
+//     'name': 'Character',
+//     'type': 'person',
+//     'role': 'hero',
+//     'children': {{ relationships | safe }}}
+
+var roleColors = {
+    "commoner": "#0ee0f7",
+    "hero": "#0dabbc",
+    "speaker":"#e5b42d",
+    "ruler":"#6037a3"
+}
+
+var typeColors = {
+    "capitol": "#FFFFFF",
+    "town": "#A8A8A8",
+    "nation": "#00C0AF"
+}
+
 console.log(relationships);
+var rootNode = d3.hierarchy(relationships)
 
 var width = 1000,
     height = 1000,
     root;
 
-var vLayout = d3.forceSimulation()
-    .force('links', d3.forceLink()
-        .id(function (d) { return d.id; })
-        .distance(function (d) { return 50 }))
-    .force('charge', d3.forceManyBody())
-    .force('center', d3.forceCenter(width / 2, height / 2));
-
 relationship_canvas = d3.select('body').append('svg')
     .attr("width", width)
     .attr("height", height);
 
-relationship_canvas.append('g').classed('clusters', true)
+relationship_canvas.append('g')
+    .classed('nodes',true)
 
-d3.select('svg .clusters').append('g').classed('links', true)
-d3.select('svg .clusters').append('g').classed('nodes', true)
+var packLayout = d3.pack()
+    .size([300, 300]);
+  
+rootNode.sum(function(d) {
+    return 5;
+  });
 
-var root = d3.hierarchy(relationships);
-var vNodes = root.descendants();
-var vLinks = root.links();
+packLayout(rootNode);
 
-var roleColors = {
-    "commoner": "#F5D040",
-    "hero": "#00FFFF"
-}
-
-var typeColors = {
-    "capitol": "#000000",
-    "town": "#A8A8A8",
-    "nation": "#00C0AF"
-}
-
-node = d3.select('svg g.nodes')
-    .selectAll('circle.node')
-    .data(root.descendants())
+d3.select('svg g')
+    .selectAll('circle')
+    .data(rootNode.descendants())
     .enter()
     .append('circle')
-    .classed('node', true)
-    .attr('cx', function (d) { return d.x; })
-    .attr('cy', function (d) { return d.y; })
-    .attr('r', function (d) { if (d.data.type == 'person') { return 10 } else { return 30 } })
-    .style("stroke", "black")
     .attr("id", function (d, i) { return "t" + String(i) })
+    .style("opacity",20)
     .attr('fill', function (d) {
-        if (d.data.type == 'person') { return roleColors[d.data.role] }
+        if (d.data.type == 'person') { return roleColors[d.data.role.split(" ")[0].toLowerCase()] }
         else { return typeColors[d.data.type] }
     })
+    .style("stroke","black")
+    .attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; })
+    .attr('r', function(d) { return d.r; })
     .on("mouseover", function (d) {
         //console.log(d3.select(this).datum().data);
         return terrain_tooltip.style("visibility", "visible")
@@ -67,60 +72,24 @@ node = d3.select('svg g.nodes')
         return terrain_tooltip.style("visibility", "hidden");
     })
 
-
-link = d3.select('svg g.links')
-    .selectAll('line.link')
-    .data(root.links())
+var nodes = d3.select('svg g')
+    .selectAll('g')
+    .data(rootNode.descendants())
     .enter()
-    .append('line')
-    .classed('link', true)
-    .attr('x1', function (d) { return d.source.x; })
-    .attr('y1', function (d) { return d.source.y; })
-    .attr('x2', function (d) { return d.target.x; })
-    .attr('y2', function (d) { return d.target.y; })
-    .style("stroke", "#ccc");
+    .append('g')
+    .attr('transform', function(d) {return 'translate(' + [d.x, d.y] + ')'})
 
-vLayout.nodes(vNodes).on('tick', tick);
+nodes
+    .append('circle')
+    .append('circle')
+    .style("opacity",20)
+    .attr('r', function(d) { return d.r; })
 
-
-// now to move them. 
-function tick() {
-    link.attr("x1", function (d) { return d.source.x; })
-        .attr("y1", function (d) { return d.source.y; })
-        .attr("x2", function (d) { return d.target.x; })
-        .attr("y2", function (d) { return d.target.y; });
-
-    node.attr("cx", function (d) { return d.x; })
-        .attr("cy", function (d) { return d.y; });
-}
-
-function color(d) {
-    return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
-}
-
-function click(d) {
-    if (!d3.event.defaultPrevented) {
-        if (d.children) {
-            d._children = d.children;
-            d.children = null;
-        } else {
-            d.children = d._children;
-            d._children = null;
-        }
-        update();
-    }
-}
-
-function flatten(root) {
-    var nodes = [], i = 0;
-
-    function recurse(node) {
-        if (node.children) node.children.forEach(recurse);
-        if (!node.id) node.id = ++i;
-        nodes.push(node);
-    }
-
-    recurse(root);
-    return nodes;
-}
-
+nodes
+    .append('text')
+    .attr('dx', -15)
+    .attr('dy', 4)
+    .style("font-size", "9px")
+    .text(function(d) {
+        return d.children === undefined ? d.data.name : '';
+    })
