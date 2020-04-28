02ebcc8298506cfd879b5d3d60ec6724f1eb5544
William Harding <william.jeffrey.harding@gmail.com>
2020-04-27 22:05:04 -0700

    Added system for special buildings

---
+++ templates/game/core/buildings/great_hall.js
@@ -0,0 +1,30 @@
+var great_hall = canvas
+            .append("g")
+            .classed("building",true)
+            .classed("great_hall",true)
+
+great_hall
+        .append("rect")
+                    .style('z-index',-1)
+                    .attr("x",(width/2) + (60 * terrData['town']["buildings"].indexOf("great_hall")))
+                    .attr("y",(height/2) + (60 * terrData['town']["buildings"].indexOf("great_hall")))
+                    .attr("width",60)
+                    .attr("height",60)
+                    .attr("affect","bump")
+                    .attr("affectText","** hit the great hall")
+                    .attr("name","great_hall")
+                    .classed("terrain", true)
+                    .classed("rect", true)
+                    .attr("stroke","black")
+                    .style("fill","black")
+                    .on("mouseover", function(d){
+                        return terrain_tooltip.style("visibility", "visible")
+                            .html("The great hall of "+terrData['town']['name']);
+                        })
+                    .on("mousemove", function(d){
+                            return terrain_tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px")
+                        .html(d.name);
+                        })
+                    .on("mouseout", function(){
+                        return terrain_tooltip.style("visibility", "hidden");
+                    })
--- templates/game/core/terrain/building.js
+++ templates/game/core/terrain/building.js
@@ -1,4 +1,6 @@
 {% include 'game/core/terrain/render_block.js' %}
 
+// not for specialist buildings. See templates/core/buildings/ for special structures
 d3.selectAll("rect")
     .style("border", "1px solid black")
+
