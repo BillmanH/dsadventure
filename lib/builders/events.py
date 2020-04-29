3cf02fa726c510fc83450eaa11175a42839ec2c1
William Harding <william.jeffrey.harding@gmail.com>
2020-04-29 08:05:51 -0700

    fixed an issue where event_features were'nt placing

--- lib/builders/events.py
+++ lib/builders/events.py
@@ -4,20 +4,24 @@ import numpy as np
 from game.models import events
 from game.lib.builders import nations
 
+
 def get_possible_events():
     return [str(i[0]) for i in events.objects.values_list('key')]
 
-def get_event(k,paths='prod'):
+
+def get_event(k, paths='prod'):
     m = events.objects.get(pk=k)
     return m
 
-def str_munge(x,a,o,t):
-        return (x.replace('{o}',str(o))
-                .replace('{a}',str(a))
-                .replace('{t}',str(t))
-                ) 
 
-def give_message(world,event,a,o,t):
+def str_munge(x, a, o, t):
+    return (x.replace('{o}', str(o))
+            .replace('{a}', str(a))
+            .replace('{t}', str(t))
+            )
+
+
+def give_message(world, event, a, o, t):
     """
     where
         message - the text string to be delivered
@@ -26,16 +30,18 @@ def give_message(world,event,a,o,t):
     """
     for i in a:
         for town in i.get_all_towns(world):
-            [p.add_message(str_munge(event.a_message, a,o,t)) for p in town.get_population(world)]
+            [p.add_message(str_munge(event.a_message, a, o, t))
+             for p in town.get_population(world)]
     for i in o:
         for town in i.get_all_towns(world):
-            [p.add_message(str_munge(event.a_message, a,o,t)) for p in town.get_population(world)]
-        
+            [p.add_message(str_munge(event.a_message, a, o, t))
+             for p in town.get_population(world)]
+
 
 def event_results(world):
     choice = np.random.choice(get_possible_events())
     event = get_event(choice)
-    #QA step, if the event calls for more subjects than there are nations
+    # QA step, if the event calls for more subjects than there are nations
     n_sub = event.n_subjects
     if n_sub > len(world.nations):
         n_sub = len(world.nations)
@@ -44,39 +50,42 @@ def event_results(world):
         n_obj = len(world.nations)
     if n_obj+n_sub > len(world.nations):
         n_obj = 1
-    a = np.random.choice(world.nations,n_sub,replace=False).tolist()
-    o = np.random.choice(world.nations,n_obj,replace=False).tolist()
-    #the actual affect of the event is done here. the event_var determines what the event actually affects.
+    a = np.random.choice(world.nations, n_sub, replace=False).tolist()
+    o = np.random.choice(world.nations, n_obj, replace=False).tolist()
+    # the actual affect of the event is done here. the event_var determines what the event actually affects.
     if event.effect_var == 'favor':
-        nations.alter_favor(a,o,float(event.effect))
-        t=''
+        nations.alter_favor(a, o, float(event.effect))
+        t = ''
     if event.effect_var == 'feature':
-        t = nations.place_feature(world,a,o,event)
+        t = nations.place_feature(world, a, o, event)
     if event.effect_var == 'buildings':
-        twn = nations.place_building(world,a,o,event)
+        twn = nations.place_building(world, a, o, event)
         t = twn.name
-    text = '{e}: ' + str_munge(event.event,a,o,t)
-    give_message(world,event,a,o,t)
+    text = '{e}: ' + str_munge(event.event, a, o, t)
+    give_message(world, event, a, o, t)
     return text
 
-#now to run through the eons and let fate happen
+# now to run through the eons and let fate happen
+
+
 def pass_through_time(world):
     all_events = []
     for e in range(world.culture.eons):
         world.year += 1
-        if np.random.random_sample()<world.culture.chaos:
-            all_events.append(event_results(world).replace('{e}',str(world.year)))
+        if np.random.random_sample() < world.culture.chaos:
+            all_events.append(event_results(
+                world).replace('{e}', str(world.year)))
         else:
-            all_events.append(f'{world.year}: nothing happend during this period.')
+            all_events.append(
+                f'{world.year}: nothing happend during this period.')
     return all_events
+
 
 def add_chaos_to_world(world):
     world.df_features['danger'] = (world.df_features['key']
-                                   .apply(lambda x: np.round(np.random.normal(world.culture.chaos, .3),3))
-                                    )
-    #water is less dangerous than land
-    world.df_features.loc[world.df_features['terrain']=='ocean','danger'] = world.df_features.loc[world.df_features['terrain']=='ocean','danger'] -.5
+                                   .apply(lambda x: np.round(np.random.normal(world.culture.chaos, .3), 3))
+                                   )
+    # water is less dangerous than land
+    world.df_features.loc[world.df_features['terrain'] == 'ocean',
+                          'danger'] = world.df_features.loc[world.df_features['terrain'] == 'ocean', 'danger'] - .5
     return world
-
-
-    
--- lib/builders/nations.py
+++ lib/builders/nations.py
@@ -3,6 +3,8 @@ import pandas as pd
 from sklearn.cluster import KMeans
 
 n_init_clusters = 30
+
+
 def cluster_nations(world):
     cities = world.df_features[world.df_features["terrain"] == "town"]
     world.nations = []
@@ -36,7 +38,6 @@ class Nation:
         self.diplomacy = {}
         self.capitol = None
 
-
     def cast_nation(self, world, k):
         world.df_features.loc[
             world.df_features["nation number"] == k, "nation"
@@ -48,7 +49,6 @@ class Nation:
         nation_name = world.culture.townNameGenerator()
         return nation_name
 
-
     def __repr__(self):
         return f"Nation of {self.name}"
 
@@ -59,7 +59,7 @@ class Nation:
         ][0]
         cap.type = "capitol"
         self.capitol = cap
-        
+
     def get_capitol(self, world):
         ts = self.get_all_towns(world)
         cap = [t for t in ts if t.type == "capitol"][0]
@@ -88,6 +88,7 @@ class Nation:
         d.columns = ["neighbor", "favor", "stance", "nation"]
         return d[["nation", "neighbor", "favor", "stance"]]
 
+
 def appoint_ruler(world, n, people):
     t = n.get_capitol(world)
     n.ruler = people.Person(world, role=f"ruler of {n.name}", location=t.key)
@@ -97,37 +98,39 @@ def appoint_ruler(world, n, people):
 # print(nation)
 
 # Politics !!
-treaties = pd.DataFrame([['sworn enemies',0],
-           ['war',.1],
-           ['tense',.3],
-           ['peace',.6],
-           ['allies',.9]], columns = ['stance','favor'])
+treaties = pd.DataFrame([['sworn enemies', 0],
+                         ['war', .1],
+                         ['tense', .3],
+                         ['peace', .6],
+                         ['allies', .9]], columns=['stance', 'favor'])
+
 
 def set_treaties(f):
-    return treaties.loc[treaties[treaties['favor']<f]['favor'].idxmax()].stance
-    
-def alter_favor(s,o,a):
+    return treaties.loc[treaties[treaties['favor'] < f]['favor'].idxmax()].stance
+
+
+def alter_favor(s, o, a):
     """
     s = the target nation(s) (obj or list). s will not change. O's favor of s will change.
     o = the nation(s) (obj or list) who's favor is change. O's favor of s will change
-    
+
     Examples:
     (a,[o]) each nation in o's favor of a is changed by s
     (a,o) o's favor of a is changed by s
     ([a],o) o's favor of each nation in a is changed by s
     s = amount of change (int)
-    
+
     national relationship with itself doesn't decay, but town and person loyalty can.
     """
     if type(o) != list:
-        o = [o]       
+        o = [o]
     for i in o:
-        if type(s)!=list:
+        if type(s) != list:
             s = [s]
         for j in s:
-            if i!=j: 
+            if i != j:
                 if j.name not in i.diplomacy.keys():
-                    i.diplomacy[j.name]={}
+                    i.diplomacy[j.name] = {}
                 if 'favor' not in i.diplomacy[j.name].keys():
                     i.diplomacy[j.name]['favor'] = .6
                 i.diplomacy[j.name]['favor'] += a
@@ -135,18 +138,26 @@ def alter_favor(s,o,a):
                     i.diplomacy[j.name]['favor'] = .01
                 if i.diplomacy[j.name]['favor'] > 1:
                     i.diplomacy[j.name]['favor'] = 1
-                i.diplomacy[j.name]['stance'] = set_treaties(i.diplomacy[j.name]['favor'])
+                i.diplomacy[j.name]['stance'] = set_treaties(
+                    i.diplomacy[j.name]['favor'])
 
-#events
-def place_building(world,a,o,event):
+# events
+
+
+def place_building(world, a, o, event):
     for n in a:
         t = np.random.choice(n.get_all_towns(world))
         t.add_building(event.key)
     return t
 
-def place_feature(world,a,o,event):
-    for n in a:
-        key = world.get_filtered_chord(n=n.name,r='key')
-        world.df_features.loc[key,'feature'] = event.key
-        #TODO do something with e (effect amount)
-        return world.df_features.loc[key,'terrain']
+
+def place_feature(world, a, o, event):
+    '''
+    features are placed randomly in a nation.
+    placed in the o nation. o is the object or recipient of the event.
+    '''
+    for n in o:
+        key = world.get_filtered_chord(n=n.name, r='key')
+        world.df_features.loc[key, 'feature'] = event.key
+        # TODO do something with e (effect amount)
+        return world.df_features.loc[key, 'terrain']
